bf7daaa3043363f0867a8950b66382e4375e2c8f
brunnonogueira <brunnonogueira4j@gmail.com>
2019-03-25 08:00:24 -0300

    Create Speality and PetType dataload

--- pet-clinic-data/src/main/java/com/nogueira4j/springframework/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/nogueira4j/springframework/petclinic/model/Vet.java
@@ -1,5 +1,16 @@
 package com.nogueira4j.springframework.petclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
 
+    private Set<Speaclity> specialities;
+
+    public Set<Speaclity> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speaclity> specialities) {
+        this.specialities = specialities;
+    }
 }
